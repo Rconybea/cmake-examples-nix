d4492f46b02532a69cfa1280c9df362749a3cdd2
Roland Conybeare <rconybeare@gmail.com>
2024-03-03 16:26:11 -0500

    + pkgs/ex4.nix

---
+++ pkgs/ex4.nix
@@ -0,0 +1,25 @@
+{
+  # dependencies
+  stdenv, cmake, pkg-config, boost182, zlib, # ... other deps here
+
+  # args
+  #   someconfigurationoption ? false
+} :
+
+stdenv.mkDerivation (finalattrs:
+  {
+    name = "cmake-examples-ex4";
+
+    # note: ../flake.nix will override this
+    src = fetchGit {
+      url = "path/to/somewhere";
+      rev = "hashofsomething";
+    };
+
+    buildInputs = [ cmake pkg-config boost182 zlib ];
+
+    installPhase = ''
+      mkdir -p $out
+      echo 'This project intentionally has no install phase'
+    '';
+  })
