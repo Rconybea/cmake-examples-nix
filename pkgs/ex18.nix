733dee162e794fe36c7da2951fd9865b4cbfa93f
Roland Conybeare <rconybeare@gmail.com>
2024-03-03 17:41:41 -0500

    + ./pkgs/ex18nix

---
+++ pkgs/ex18.nix
@@ -0,0 +1,23 @@
+{
+  # dependencies
+  stdenv, cmake, catch2, pkg-config, python311Packages, boost182, zlib, # ... other deps here
+
+  # args
+  #   someconfigurationoption ? false
+} :
+
+stdenv.mkDerivation (finalattrs:
+  {
+    name = "cmake-examples-ex18";
+
+    # note: ../flake.nix will override this
+    src = fetchGit {
+      url = "path/to/somewhere";
+      rev = "hashofsomething";
+    };
+
+    # run unit tests
+    doCheck = true;
+
+    buildInputs = [ cmake pkg-config python311Packages.pybind11 catch2 boost182 zlib ];
+  })
