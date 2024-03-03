660cd651aafea5b23ce259323264c84288218e09
Roland Conybeare <rconybeare@gmail.com>
2024-03-03 17:17:56 -0500

    + ./pkgs/ex10.nix

---
+++ pkgs/ex10.nix
@@ -0,0 +1,20 @@
+{
+  # dependencies
+  stdenv, cmake, catch2, pkg-config, boost182, zlib, # ... other deps here
+
+  # args
+  #   someconfigurationoption ? false
+} :
+
+stdenv.mkDerivation (finalattrs:
+  {
+    name = "cmake-examples-ex10";
+
+    # note: ../flake.nix will override this
+    src = fetchGit {
+      url = "path/to/somewhere";
+      rev = "hashofsomething";
+    };
+
+    buildInputs = [ cmake pkg-config boost182 zlib ];
+  })
