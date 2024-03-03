32efc46c20382d74ff85c5b1dcd8395385c3d082
Roland Conybeare <rconybeare@gmail.com>
2024-03-03 15:59:24 -0500

    + pkgs/ex1c.nix

---
+++ pkgs/ex1c.nix
@@ -0,0 +1,40 @@
+{
+  # dependencies
+  stdenv, cmake,  # ... other deps here
+
+  # args
+
+  #   attrset for fetching source code.
+  #    { type, owner, repo, ref }
+  #
+  #   e.g. type="github", owner="rconybea", repo="cmake-examples", ref="ex1"
+  #
+  #   see [[../flake.nix]]
+  #
+#  cmake-examples-ex1c-path,
+
+  # someconfigurationoption ? false
+} :
+
+stdenv.mkDerivation (finalattrs:
+  {
+    name = "cmake-examples-ex1c";
+
+    # note: ../flake.nix will override this
+    src = fetchGit {};
+
+#    src = cmake-examples-ex1c-path;
+
+#    src = (fetchGit {
+#      url = "https://github.com/rconybea/cmake-examples";
+#      #ref = "ex1";
+#      #rev = "c0472c9d7e4d2c53bfb977d3182380832fe96645";
+#    });
+
+    buildInputs = [ cmake ];
+
+    installPhase = ''
+      mkdir -p $out
+      echo 'This project intentionally has no install phase'
+    '';
+  })
