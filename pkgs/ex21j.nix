e2f58089df86760ee5891263a2ce0db531ed7210
Roland Conybeare <rconybeare@gmail.com>
2024-03-03 18:34:15 -0500

    + ./pkgs/ex21j.nix

---
+++ pkgs/ex21j.nix
@@ -0,0 +1,23 @@
+{
+  # dependencies
+  stdenv, cmake, catch2, pkg-config, python3Packages, boost, zlib, # ... other deps here
+
+  # args
+  #   someconfigurationoption ? false
+} :
+
+stdenv.mkDerivation (finalattrs:
+  {
+    name = "cmake-examples-ex21j";
+
+    # note: ../flake.nix will override this
+    src = fetchGit {
+      url = "path/to/somewhere";
+      rev = "hashofsomething";
+    };
+
+    # run unit tests
+    doCheck = true;
+
+    buildInputs = [ cmake pkg-config python3Packages.pybind11 catch2 boost zlib ];
+  })
